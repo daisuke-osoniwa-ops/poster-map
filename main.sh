9b7938f7aa0eec8e368957d291deee2ccf67dbbe
daisuke-osoniwa-ops <daisuke-osoniwa@kino-kuniya.com>
2025-12-11 13:13:08 +0900

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,29 +1,15 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 set -euo pipefail
 
-cd ~/tokyo2024-poster-map/ #Path to the folder
-
-git pull
-
-# Download latest CSV from spreadsheet datbase
-curl -sL "https://docs.google.com/spreadsheets/d/e/2PACX-1vS1W4aMccgKRWVHDyqtaSIC8duUonV99Yfa2_sDc9nEehWwc6Mvdm-B6UxOi3F0s9mi3bx9sQinSaq_/pub?gid=474539051&single=true&output=csv" > public/data/all.csv
+# 1. 準備（必要なソフトを入れる）
 pip install -r python-requirements.txt
-# all.json
+
+# 2. データのダウンロード（あなたのスプレッドシートURL）
+curl -sL "https://docs.google.com/spreadsheets/d/e/2PACX-1vS1W4aMccgKRWVHDyqtaSIC8duUonV99Yfa2_sDc9nEehWwc6Mvdm-B6UxOi3F0s9mi3bx9sQinSaq_/pub?gid=474539051&single=true&output=csv" > public/data/all.csv
+
+# 3. データを地図用に変換する
 python3 csv2json_small.py public/data/all.csv public/data/
 
-# summary.json
+# 4. 集計処理（念のため実行）
 python3 summarize_progress.py ./public/data/summary.json
-
-# summary_absolute.json
 python3 summarize_progress_absolute.py ./public/data/summary_absolute.json
-
-git add -N .
-
-if ! git diff --exit-code --quiet
-then
-    git add .
-    git commit -m "Update"
-    git push
-    source .env
-    npx netlify-cli deploy --prod --message "Deploy" --dir=./public --auth $NETLIFY_AUTH_TOKEN
-fi
